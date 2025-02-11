61b0474b5102d9b5b268836276a7a532631b2102
pasha_danilevich <danilevitch.pasha@yandex.ru>
2025-02-11 14:34:30 +0300

    Игнорирование *.sql и удаление их из индекса

--- v_demand/privileges_2024_2.sql
+++
@@ -1,8 +0,0 @@
-select
-    p.person_id,
-    to_char(p.personprivilege_begdate, 'YYYY-MM-DD') as "Дата включения в регистр ЛЛО",
-    to_char(p.personprivilege_begdate, 'YYYY-MM') as "Месяц года",
-    l.org_nick "МО"
-    from v_personprivilege p
-    left join v_lpu l using (lpu_id)
-where 1=1
